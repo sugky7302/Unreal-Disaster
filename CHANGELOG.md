60f1c526459e58a0338a2b9e6a1fad058e3b666b
Arvin Yang <sugky7302@gmail.com>
2022-10-17 14:31:46 +0800

    簡化 main

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,13 @@
 # CHANGELOG
 
-## [0.2.0.3][0.2.0] - 2022-10-17 - 配置地圖
+## [0.2.0.4][0.2.0] - 2022-10-17 - 配置地圖
 - 使用內置 JAPI 來開發地圖。
 - 使用 lua 來開發遊戲。
 - 以 w3x2lni 來打包及優化地圖。
 - 在 scripts/ 裡面撰寫程式碼。
+
+### Changed:
+- 簡化 main.lua。
 
 [0.2.0]:https://github.com/sugky7302/Unreal-Disaster/commit/77793baea0fa3a118218cab192f04411f2b87a31
 
--- map/main.lua
+++ map/main.lua
@@ -1,43 +1,65 @@
--- 此module啟用lua訊息框，並加載路徑，使後續module在require時，能夠省去父路徑
 local require = require
 local runtime = require 'jass.runtime'
 local console = require 'jass.console'
 
+-- 重載 print，自動轉換編碼
+print = console.write
+
+-- 將 handle 等級設為 0（地圖中所有的 handle 均使用 table 封裝）
+runtime.handle_level = 0
+
+-- 關閉等待
+runtime.sleep = false
+
+--* 不設定成 local 是因為要讓所有程式都可以訪問
+ENV = {
+    -- 開發模式：debug、release
+    MODE = "debug",
+    -- 版本號
+    VERSION = "0.2.0",
+    -- 顯示錯誤訊息
+    error = function(msg)
+        print("---------------------------------------")
+        print(tostring(msg) .. "\n")
+        print(debug.traceback())
+        print("---------------------------------------")
+    end
+}
+
+---於 debug 模式下顯示訊息
+---@param msg string - 訊息
+function Debug(msg)
+    if ENV.MODE == "debug" then
+        print(msg)
+    end
+end
+
+-- 調試模式
+if ENV.MODE == 'debug' then
+    -- 打開控制台
+    console.enable = true
+
+    -- 開啟調試器端口
+    runtime.debugger = 4279
+end
+
+---重載 runtime 的錯誤訊息
+---@param msg string - 錯誤訊息
+function runtime.error_handle(msg)
+    ENV.error(msg)
+end
+
+---安全地匯入模塊，並簡化模塊名稱，以便未來調整位置
+---* 不設定成 local 是因為要讓所有程式都可以訪問
+---@param path string - 文件路徑 
+---@return unknown - 如果文件有 return 會回傳
 function Import(path)
     return require('scripts.' .. path)
 end
 
-local Console = {}
--- 判斷是否為發布版本
-Console.release = not pcall(require, 'lua.currentpath')
--- 設定版本號
-Console.version = '0.1.0'
--- 打開控制台
-if not Console.release then
-    console.enable = true
-end
--- 重載print，自動轉換編碼
-print = console.write
--- 將handle等級設為0(地圖中所有的 handle 均使用 table 封裝)
-runtime.handle_level = 0
--- 關閉等待
-runtime.sleep = false
-function Console.error_handle(msg)
-    print("---------------------------------------")
-    print(tostring(msg) .. "\n")
-    print(debug.traceback())
-    print("---------------------------------------")
-end
--- 顯示錯誤訊息
-function runtime.error_handle(msg)
-    Console.error_handle(msg)
-end
-if not Console.release then
-    -- 調試器端口
-    runtime.debugger = 4279
-end
-
+-- 入口函數
 local function main()
-    Import("abc.wenhao_texture")
+    Debug(table.concat({"---Start Console---\nLua version: ", runtime.version, "\nMap version: ", ENV.VERSION, "\nMode: ", ENV.MODE, "\n-------------------"}))
+    Import("test.wenhao_texture")
 end
 main()
